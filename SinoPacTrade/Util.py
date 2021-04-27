98a4e9d0cc5ff35e54e2208e88498a3a08185be5
Tingchu <yobradyo@gmail.com>
2021-04-28 00:22:12 +0800

    Add GUI chart related codes (but unused)

--- SinoPacTrade/Util.py
+++ SinoPacTrade/Util.py
@@ -1,4 +1,6 @@
 import datetime
+import matplotlib.pyplot as plt
+import numpy
 
 def log(msg, level="", stdout=True, dump=True):
     dateAndTime = datetime.datetime.now().strftime("%Y/%m/%d, %H:%M:%S")
@@ -28,3 +30,41 @@ def getPositions(api):
             closingPrices.append(position["ContractAverPrice"])
 
     return positionAction, closingPrices
+
+def initializeChart(lines, axes):
+    # function to show the plot
+    # plt.show()
+
+    # axes = plt.gca()
+
+    # plotting the points
+    # plt.plot(xCoordinates, yCoordinates, color='green', linestyle='-', linewidth = 2, markerfacecolor='black')
+    # lines, = axes.plot([], [], color='green', linestyle='-', linewidth = 2, marker='o', markerfacecolor='black', markersize=8)
+    xdata = []
+    ydata = []
+    lines.set_xdata(xdata)
+    lines.set_ydata(ydata)
+
+    # setting x and y axis range
+    # plt.ylim(1,8)
+    # plt.xlim(1,8)
+
+    # naming the axes
+    plt.xlabel('x axis')
+    plt.ylabel('y axis')
+
+    # giving a title to my graph
+    plt.title('Dealed prices')
+
+def updateChart(lines, axes, xValue, yValue):
+    print(f"{xValue} {yValue}")
+    xdata = lines.get_xdata()
+    xdata.append(xValue)
+    ydata = lines.get_ydata()
+    ydata.append(yValue)
+    axes.set_xlim(min(xdata), max(xdata))
+    axes.set_ylim(min(ydata), max(ydata))
+    lines.set_xdata(xdata)
+    lines.set_ydata(ydata)
+    plt.draw()
+    plt.pause(1e-17)
